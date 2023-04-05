1bad63e8faa3d774826fedfff722c6fb56ece0f3
rakibsarowar <rakibsarowar@gmail.com>
2023-04-05 21:34:26 +0600

    6_use loader to load data and create shared loader

---
+++ src/Loaders/cartProductLoader.js
@@ -0,0 +1,9 @@
+const cartProductsLoader = async () => {
+const loadedProducts = await fetch('products.json');
+const products = await loadedProducts.json();
+console.log(products)
+return products;
+
+}
+
+export default cartProductsLoader;
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products)
     return (
         <div className='shop-container'>
             <div className='products-container'>
-            <h1>Orders Page</h1>
+            <h1>Orders Page: {products.length}</h1>
             </div>
             <div className='cart-container'>
                 <Cart cart={[]}></Cart>
