5ad8b7bcc92507aa4dccfe749a6ee8a65a6071f2
rakibsarowar <rakibsarowar@gmail.com>
2023-04-05 21:22:10 +0600

    5_create orders page and used cart component in empty array props

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
     return (
-        <div>
+        <div className='shop-container'>
+            <div className='products-container'>
             <h1>Orders Page</h1>
+            </div>
+            <div className='cart-container'>
+                <Cart cart={[]}></Cart>
+            </div>
         </div>
     );
 };
