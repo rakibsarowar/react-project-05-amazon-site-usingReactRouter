e7e89de68100fb74432d4bd37454b883de855d10
rakibsarowar <rakibsarowar@gmail.com>
2023-04-05 21:04:56 +0600

    setting children component

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Shop></Shop>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      }
+    ]
   }
 ])
 
