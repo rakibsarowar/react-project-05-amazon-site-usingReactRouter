d8dd4925102861cb737a9cd96139dd7b74ed1665
rakibsarowar <rakibsarowar@gmail.com>
2023-04-06 19:31:52 +0600

    declare a state

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import Product from '../Product/Product';
@@ -6,21 +6,22 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
 
 const Orders = () => {
-    const cart = useLoaderData();
-    console.log(cart)
+    const savedCart = useLoaderData();
+    const [] = useState(savedCart)
+    // console.log(cart)
     return (
         <div className='shop-container'>
             <div className='review-container'>
-            <h1>Orders Page: {cart.length}</h1>
+            <h1>Orders Page: {savedCart.length}</h1>
             {
-                cart.map(Product=> <ReviewItem
+                savedCart.map(Product=> <ReviewItem
                 key={Product.id}
                 Product={Product}
                 ></ReviewItem>)
             }
             </div>
             <div className='cart-container'>
-                <Cart cart={cart}></Cart>
+                <Cart cart={savedCart}></Cart>
             </div>
         </div>
     );
